5eb4f066dc2e17c755f8d9f2729daec0c3d574d5
ruby -v ruby -v <ajhofferber@gmail.com>
2016-04-27 17:40:47 -0400

    NAILED IT

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,29 @@
-function scrollIt(offsetHeight){
-
-  $(window).scroll(function(){
-
-
-    $(".element-1").css("bottom", offsetHeight - ($(this).scrollTop()));
-    console.log($(this).scrollTop());
-    // console.log($(".element-1").css("top"));
-     var winHeight = $(window).height();
-     var winWidth = $(window).width();
-    //  console.log($(window).scrollTop())
-    //  console.log("height: " + winHeight + " AND width: " + winWidth);
-  });
-}
+// function scrollIt(offsetHeight){
+//
+//   $(window).scroll(function(){
+//
+//
+//     $(".element-1").css("bottom", ( offsetHeight - ($(this).scrollTop()) ));
+//     console.log($(".element-1").css("bottom"));
+//     // console.log($(".element-1").css("top"));
+//      var winHeight = $(window).height();
+//      var winWidth = $(window).width();
+//     //  console.log($(window).scrollTop())
+//     //  console.log("height: " + winHeight + " AND width: " + winWidth);
+//   });
+// }
 
 $(document).ready(function(){
 
   var winHeight = $(window).height();
   var picHeight = $('.element-1').height()
-  var offsetHeight = winHeight + picHeight
-  $('.element-1').css('bottom', -offsetHeight);
+  var offsetHeight = winHeight + picHeight *.9
+  $('.element-2').css('margin-top', offsetHeight*1.8)
+  console.log($('.element-2').css('margin-top'));
+  $('.element-1').css('bottom', -(offsetHeight) );
   console.log($('.element-1').css('bottom'));
-  scrollIt(offsetHeight)
+
+  $(window).scroll(function(){
+    $(".element-1").css("bottom", Math.min( 0, (-(offsetHeight)+(($(this).scrollTop())*.8)) ) );
+  });
 })
